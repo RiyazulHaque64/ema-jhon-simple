1409bf98386de4ae2d48d20d962ed1b784cc1550
RiyazulHaque64 <riyazulhaque64@gmail.com>
2023-03-29 16:31:19 +0600

    first commit

---
+++ src/components/Shop/Shop.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Shop.css';
+
+const Shop = () => {
+    return (
+        <div className='shop-container'>
+            <div className="product-container">
+                <h2>Product comming here</h2>
+            </div>
+            <div className="order-summery">
+                <h4>Order summery</h4>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
