e14295be3f446160959052631f2d806adf2db51e
RiyazulHaque64 <riyazulhaque64@gmail.com>
2023-04-06 13:00:03 +0600

    Complete order page

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,26 +2,30 @@ import React from 'react';
 import "./Cart.css";
 
 const Cart = ({ cart }) => {
-    console.log(cart)
     let totalPrice = 0;
     let totalShipping = 0;
-
+    let quantity = 0;
     for (const product of cart) {
-        totalPrice += product.price;
+        // if (product.quantity === 0) {
+        //     product.quantity = 1;
+        // }
+        // Shortcut: product.quantity = product.quantity || 1;
+        totalPrice = totalPrice + product.price * product.quantity;
         totalShipping += product.shipping;
+        quantity += product.quantity;
     }
 
     const tax = totalPrice * 5 / 100;
 
     const total = (totalPrice + totalShipping + tax).toFixed(2);
     return (
-        <div className='cart'>
-            <h4>Order summery</h4>
-            <p>Seleted items: {cart.length}</p>
-            <p>Total Price: ${totalPrice}</p>
-            <p>Total shipping charge: ${totalShipping}</p>
-            <p>Tax: ${tax.toFixed(2)}</p>
-            <h6>Grand Total: {total}</h6>
+        <div className='bg-orange-200 p-5 rounded-lg sticky top-3'>
+            <h4 className='text-center text-2xl font-semibold mb-8'>Order summery</h4>
+            <p className='text-xl mb-2'>Seleted items: {quantity}</p>
+            <p className='text-xl mb-2'>Total Price: ${totalPrice}</p>
+            <p className='text-xl mb-2'>Total shipping charge: ${totalShipping}</p>
+            <p className='text-xl mb-2'>Tax: ${tax.toFixed(2)}</p>
+            <h6 className='text-2xl font-semibold'>Grand Total: {total}</h6>
         </div>
     );
 };
--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './Product.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 
@@ -7,15 +6,17 @@ const Product = (props) => {
     const { img, name, price, seller, ratings } = props.product;
     const handleAddToCart = props.handleAddToCart;
     return (
-        <div className='product'>
-            <div className="product-info">
-                <img src={img} alt="product-image" />
-                <h4>{name}</h4>
-                <h6>${price}</h6>
-                <p>Manufacturer: {seller}</p>
-                <p>Rating: {ratings}</p>
+        <div className='border border-gray-200 rounded-lg flex flex-col justify-between'>
+            <div>
+                <img className='w-66 h-66 p-2 rounded-2xl' src={img} alt="product-image" />
+                <div className="p-4">
+                    <h4 className='text-2xl font-semibold mb-2'>{name}</h4>
+                    <h6 className='text-xl mb-6'>${price}</h6>
+                    <p className='mb-2'>Manufacturer: {seller}</p>
+                    <p>Rating: {ratings}</p>
+                </div>
             </div>
-            <button onClick={() => handleAddToCart(props.product)}>Add to Cart <FontAwesomeIcon icon={faShoppingCart} /></button>
+            <button className='bg-orange-200 w-full py-3 hover:bg-orange-300 duration-200' onClick={() => handleAddToCart(props.product)}>Add to Cart <FontAwesomeIcon icon={faShoppingCart} /></button>
         </div>
     );
 };
